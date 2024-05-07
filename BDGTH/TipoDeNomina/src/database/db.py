92e95f841f35be0648408e517cee2c29d0757677
JuanCarlos97-web <jchernandez97@outlook.es>
2024-05-07 12:37:41 -0600

    Se agregan variables de entorno

--- BDGTH/TipoDeNomina/src/database/db.py
+++ BDGTH/TipoDeNomina/src/database/db.py
@@ -1,6 +1,7 @@
 import pyodbc
 
 from flask import Blueprint
+from decouple import config
 
 
 
@@ -11,11 +12,20 @@ class ConexionSQL:
 
         try:
 
-                # Configuración de la conexión a SQL Server
+            '''
+                           # Configuración de la conexión a SQL Server
             server = 'LAPTOP-PP6N1N88\SQLEXPRESS'
             database = 'BDGTH'
             username = 'sa'
             password = 'artesanos10'
+            cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+            '''
+
+                # Configuración de la conexión a SQL Server
+            server = config('SQL_HOST')
+            database = config('SQL_DB')
+            username = config('SQL_USER')
+            password = config('SQL_PASSWORD')
             cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
             
             return cnxn
